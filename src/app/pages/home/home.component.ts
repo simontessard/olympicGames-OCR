ef122c4e7d80da0fff263bae921942710a2df1fd
Simon Tessard <simon.tessard@gmail.com>
2023-10-06 12:34:14 +0200

    Pie Chart display without mocked data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,6 +10,50 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> | undefined;
 
+  countries = [
+    {
+      "name": "Germany",
+      "value": 40632,
+      "extra": {
+        "code": "de"
+      }
+    },
+    {
+      "name": "United States",
+      "value": 50000,
+      "extra": {
+        "code": "us"
+      }
+    },
+    {
+      "name": "France",
+      "value": 36745,
+      "extra": {
+        "code": "fr"
+      }
+    },
+    {
+      "name": "United Kingdom",
+      "value": 36240,
+      "extra": {
+        "code": "uk"
+      }
+    },
+    {
+      "name": "Spain",
+      "value": 33000,
+      "extra": {
+        "code": "es"
+      }
+    },
+    {
+      "name": "Italy",
+      "value": 35800,
+      "extra": {
+        "code": "it"
+      }
+    }
+  ]
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
