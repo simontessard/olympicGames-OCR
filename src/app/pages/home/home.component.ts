5843c1bf8312b61a4f8e9e015e7bf7d5f4cba07e
Simon Tessard <simon.tessard@gmail.com>
2023-10-06 11:49:50 +0200

    Updated project with dependencies

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -8,7 +8,7 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics$: Observable<any> | undefined;
 
   constructor(private olympicService: OlympicService) {}
 
