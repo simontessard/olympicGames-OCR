737bc25fa81c560ee936e3b4f47d435a45f95e5e
Simon Tessard <simon.tessard@gmail.com>
2023-10-06 16:51:01 +0200

    Displaying medalsCount from mocked data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { Olympic } from 'src/app/core/models/Olympic';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -9,54 +11,31 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> | undefined;
+  public countries2: any[] = [];
 
-  countries = [
-    {
-      "name": "Germany",
-      "value": 40632,
-      "extra": {
-        "code": "de"
-      }
-    },
-    {
-      "name": "United States",
-      "value": 50000,
-      "extra": {
-        "code": "us"
-      }
-    },
-    {
-      "name": "France",
-      "value": 36745,
-      "extra": {
-        "code": "fr"
-      }
-    },
-    {
-      "name": "United Kingdom",
-      "value": 36240,
-      "extra": {
-        "code": "uk"
-      }
-    },
-    {
-      "name": "Spain",
-      "value": 33000,
-      "extra": {
-        "code": "es"
-      }
-    },
-    {
-      "name": "Italy",
-      "value": 35800,
-      "extra": {
-        "code": "it"
-      }
-    }
-  ]
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
+    this.olympics$ = this.olympicService.getOlympics().pipe(
+      filter((data: Olympic[]) => !!data), // Wait for data to be defined
+      map((data: Olympic[]) => {
+        return data.map((item: Olympic) => ({
+          name: item.country,
+          // Get sum of all medals won across all participations
+          value: item.participations.reduce((total, participation) => total + participation.medalsCount, 0),
+        }));
+      })
+    );
+  }
+
+  ngAfterViewInit(): void {
+    // Now data has been loaded and olympics$ is defined
+    if (this.olympics$) {
+      this.olympics$.subscribe(transformedData => {
+        this.countries2 = transformedData;
+      });
+    } else {
+      console.error('this.olympics$ is undefined');
+    }
   }
 }
