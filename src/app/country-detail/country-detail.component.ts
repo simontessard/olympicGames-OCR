18d5b1cf2ccdfaaf3fd9a507dff3dcea4f2a1b49
Simon Tessard <simon.tessard@gmail.com>
2023-10-16 22:10:56 +0200

    Optimizations country page

--- src/app/country-detail/country-detail.component.ts
+++ src/app/country-detail/country-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 import { Olympic } from 'src/app/core/models/Olympic';
+import { Participation } from 'src/app/core/models/Participation';
 import { filter, map } from 'rxjs/operators';
 
 @Component({
@@ -14,9 +15,6 @@ export class CountryDetailComponent {
   countryName: string = '';
   public countryObservable: Observable<any> | undefined;
   public countryData: Olympic[] = [];
-  public nbEntries: number = 0;
-  public totalMedals: number = 0;
-  public totalAthletes: number = 0;
 
   constructor(private olympicService: OlympicService, private route: ActivatedRoute) { }
 
@@ -29,28 +27,21 @@ export class CountryDetailComponent {
       filter((data: Olympic[]) => !!data), // Wait for data to be defined
       map((data: Olympic[]) => {
         const countryData = data.find(item => item.country === this.countryName); // Trouvez le pays par son nom
+        const valueData: any[] = countryData?.participations.map(participation => ({
+          name: participation.year,
+          series: [{
+            name: participation.year,
+            value: participation.medalsCount
+          }]
+        })) || [];
         return {
           name: countryData?.country || '', // Utilisez le nom du pays
-          value: countryData?.participations,
           medalsCount: countryData?.participations.reduce((total, participation) => total + participation.medalsCount, 0),
           athleteCount: countryData?.participations.reduce((total, participation) => total + participation.athleteCount, 0),
+          value: countryData?.participations,
+          data: valueData,
         };
       })
     );
-  }
-  ngAfterViewInit(): void {
-    // Now data has been loaded and olympics$ is defined
-    if (this.countryObservable) {
-      this.countryObservable.subscribe(transformedData => {
-        this.countryData = transformedData;
-        console.log(transformedData)
-        this.nbEntries = transformedData.value.length;
-        this.totalMedals = transformedData.medalsCount;
-        this.totalAthletes = transformedData.athleteCount;
-        console.log(this.countryData)
-      });
-    } else {
-      console.error('this.olympics$ is undefined');
-    }
   }
 }
